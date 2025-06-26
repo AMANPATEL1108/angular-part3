a77f5f51197491b1be27f9518a8c853184385d36
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-06-26 08:55:11 +0530

    If Else Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,36 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular-part3';
+  toggleDiv = false;
+
+  display = true;
+
+  hideShowDiv() {
+    this.display = !this.display;
+
+    // if (this.display == true) {
+    //   this.display = false;
+    // } else {
+    //   this.display = true;
+    // }
+  }
+
+  // showDiv() {
+  //   console.log('Called');
+  //   this.display = true;
+  // }
+
+  toggle2() {
+    this.toggleDiv = !this.toggleDiv;
+
+    // if (this.toggleDiv == true) {
+    //   this.toggleDiv = false;
+    // } else {
+    //   this.toggleDiv = true;
+    // }
+  }
 }
